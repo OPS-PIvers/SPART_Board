fb731401f6288673f1e102944a8db55207bdc36a
Paul Ivers <138139858+OPS-PIvers@users.noreply.github.com>
2026-03-18 14:11:43 +0000

    fix: allow Earth Networks URL in weather proxy and fix functions tests configuration

--- functions/vitest.config.ts
+++ functions/vitest.config.ts
@@ -4,5 +4,13 @@ export default defineConfig({
   test: {
     environment: 'node',
     globals: true,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/lib/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+    ],
   },
 });
